bd8950c3b85f2e342ca427e27479ddb8cbc90423
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-03-05 20:20:17 -0600

    created filter for products with path variable

--- src/main/java/com/naveen/productsapi/repository/ModelRepo.java
+++ src/main/java/com/naveen/productsapi/repository/ModelRepo.java
@@ -3,5 +3,10 @@ package com.naveen.productsapi.repository;
 import com.naveen.productsapi.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ModelRepo extends JpaRepository<Model, Long> {
+
+    List<Model> findByGender(String gender);
 }
--- src/main/java/com/naveen/productsapi/repository/ProductRepo.java
+++ src/main/java/com/naveen/productsapi/repository/ProductRepo.java
@@ -1,7 +1,12 @@
 package com.naveen.productsapi.repository;
 
+import com.naveen.productsapi.model.Model;
 import com.naveen.productsapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByModelIn(List<Model> models);
+
 }
